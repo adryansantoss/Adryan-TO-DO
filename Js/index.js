3a1ce634b89ecdf0ffb5899580d8e9e6dfc5a864
adryansantoss <53420986+adryansantoss@users.noreply.github.com>
2020-10-21 13:00:33 -0300

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -5,6 +5,7 @@ window.onload = function() {
     var container = document.getElementById('toDo-container')
     var add = document.getElementById('todDo-add')
 
+    //ADD,REMOVE,MARK TO-DO
     add.addEventListener('click', () => {
         if (title.value != "" && content.value != "") {
             var toDoDiv = document.createElement('div')
@@ -12,15 +13,18 @@ window.onload = function() {
             var toDoTitle = document.createElement('h1')
             var toDoRemove = document.createElement('button')
             var toDoMarked = document.createElement('button')
-            toDoRemove.innerText = 'DELETE'
+            toDoRemove.innerText = "DELETE"
             toDoRemove.id = 'toDo-remove'
             toDoMarked.innerText = 'FINALIZED'
             toDoMarked.id = 'toDo-marked'
             toDoRemove.classList.add('toDo-remove')
             toDoMarked.classList.add('toDo-finalized')
+            toDoTitle.classList = 'toDoTitle'
+            toDoTitle.id = 'toDoTitle'
             toDoTitle.innerText = title.value
             toDoTitle.style.textTransform = 'uppercase'
             toDoTitle.style.fontSize = '22px'
+            toDoParagraph.style.fontSize = '13px'
             toDoParagraph.innerText = content.value
             toDoDiv.classList.add('to-dos')
             container.appendChild(toDoDiv)
@@ -36,12 +40,65 @@ window.onload = function() {
         })
 
         toDoMarked.addEventListener('click', () => {
-            toDoTitle.style.textDecoration = 'line-through'
-            toDoParagraph.style.textDecoration = 'line-through'
+            if (toDoTitle.style.textDecoration == 'none') {
+                toDoTitle.style.textDecoration = 'line-through'
+                toDoParagraph.style.textDecoration = 'line-through'
+            } else {
+                toDoTitle.style.textDecoration = 'none'
+                toDoParagraph.style.textDecoration = 'none'
+            }
         })
-
     })
 
+
+    document.addEventListener('keydown', (e) => {
+        if (title.value != "" && content.value != "") {
+            if (e.key === 'Enter') {
+                var toDoDiv = document.createElement('div')
+                var toDoParagraph = document.createElement('p')
+                var toDoTitle = document.createElement('h1')
+                var toDoRemove = document.createElement('button')
+                var toDoMarked = document.createElement('button')
+                toDoRemove.innerText = "DELETE"
+                toDoRemove.id = 'toDo-remove'
+                toDoMarked.innerText = 'FINALIZED'
+                toDoMarked.id = 'toDo-marked'
+                toDoRemove.classList.add('toDo-remove')
+                toDoMarked.classList.add('toDo-finalized')
+                toDoTitle.classList = 'toDoTitle'
+                toDoTitle.id = 'toDoTitle'
+                toDoTitle.innerText = title.value
+                toDoTitle.style.textTransform = 'uppercase'
+                toDoTitle.style.fontSize = '22px'
+                toDoParagraph.style.fontSize = '13px'
+                toDoParagraph.innerText = content.value
+                toDoDiv.classList.add('to-dos')
+                container.appendChild(toDoDiv)
+                toDoDiv.appendChild(toDoTitle)
+                toDoDiv.appendChild(toDoParagraph)
+                toDoDiv.appendChild(toDoRemove)
+                toDoDiv.appendChild(toDoMarked)
+                title.value = ''
+                content.value = ''
+            }
+
+            toDoRemove.addEventListener('click', () => {
+                container.removeChild(toDoDiv)
+            })
+
+            toDoMarked.addEventListener('click', () => {
+                if (toDoTitle.style.textDecoration == 'none') {
+                    toDoTitle.style.textDecoration = 'line-through'
+                    toDoParagraph.style.textDecoration = 'line-through'
+                } else {
+                    toDoTitle.style.textDecoration = 'none'
+                    toDoParagraph.style.textDecoration = 'none'
+                }
+            })
+        }
+    });
+
+    //DATE
     var toDotime = new Date();
     var dia = toDotime.getDate()
     var mes = toDotime.getMonth() + 1
